b59f97a47fb1858c0d53c12f38e1b5398c4ad139
drip <118680430+websitecreatorx@users.noreply.github.com>
2024-06-20 10:33:23 +0200

    Delete Javascript Files directory

--- Javascript Files/register.js
+++
@@ -1,6 +0,0 @@
-const RegisterPasswordForm = document.getElementById('registerform')
-
-function register(){
-    alert('Register Successful');
-    RegisterPasswordForm.setAttribute('action', '/HTML Files/login.html')
-}
