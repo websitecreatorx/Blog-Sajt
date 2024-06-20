2445f50098e4143cdbad3f36fe1d17161bedbc70
drip <118680430+websitecreatorx@users.noreply.github.com>
2024-06-20 10:37:17 +0200

    Delete Javascript Files directory

--- Javascript Files/resetpassword.js
+++
@@ -1,6 +0,0 @@
-const ResetPasswordForm = document.getElementById('resetpassword-form');
-
-function resetPassword(){
-    alert('Password Change Successful');
-    ResetPasswordForm.setAttribute('action', '/HTML Files/login.html')
-}
