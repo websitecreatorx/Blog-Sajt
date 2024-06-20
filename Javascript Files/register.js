f47aa941ab58c3169d783fa28f77b1805568b97c
drip <118680430+websitecreatorx@users.noreply.github.com>
2024-06-20 10:03:01 +0200

    Add files via upload

---
+++ Javascript Files/register.js
@@ -0,0 +1,6 @@
+const RegisterPasswordForm = document.getElementById('registerform')
+
+function register(){
+    alert('Register Successful');
+    RegisterPasswordForm.setAttribute('action', '/HTML Files/login.html')
+}
